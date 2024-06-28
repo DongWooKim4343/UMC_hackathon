5bf5ca829f7b3158c0a95d844c87a88ea297eedc
DongWooKim4343 <kd6904@naver.com>
2024-06-28 23:35:20 +0900

    커밋 메시지

--- src/main/java/UMC/spring/Application.java
+++ src/main/java/UMC/spring/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+//b
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
